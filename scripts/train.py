c43ee9176d561cb6f5a6a068d8d2dbff6e89c0fd
Wongkraiwich Chuenchomphu <wongkraiwich@inedible.dev>
2024-07-30 01:04:23 +0700

    add mtnlog as dep

--- scripts/train.py
+++ scripts/train.py
@@ -6,11 +6,11 @@ import os
 import psutil
 
 import pandas as pd
+from mtnlog import JSONLogger, PerformanceLogger
 from llm_finetune.arguments import (DataArguments, LoggingArguments,
                                     ModelArguments, TrainingArguments)
 from llm_finetune.dataset import make_supervised_data_module
 from llm_finetune.trainer import EpochTimingCallback
-from nvitop import ResourceMetricCollector, Device
 from peft import LoraConfig
 from torch import bfloat16
 from transformers import (AutoModelForCausalLM, AutoTokenizer,
@@ -42,113 +42,12 @@ def combine_keys_to_one_layer_dict(d, sep="."):
     return new_dict
 
 
-def serialize(obj):
-    """Custom serialization for non-serializable objects."""
-    if isinstance(obj, dict):
-        return {k: serialize(v) for k, v in obj.items()}
-    if isinstance(obj, list):
-        return [serialize(v) for v in obj]
-    if isinstance(obj, (int, float, str, bool, type(None))):
-        return obj
-    # Convert non-serializable objects to their string representation
-    return str(obj)
-
-
 def deactivate_and_collect(collector, df, metric, tag=None):
     """Deactivates the collector and collects metrics."""
     df_metrics = pd.DataFrame.from_records(metric, index=[len(df)])
     updated_df = pd.concat([df, df_metrics], ignore_index=True)
     collector.deactivate(tag=tag)
     return updated_df
-
-
-class PerformanceLogger:
-    """Performance logger class."""
-
-    def __init__(self, log_dir, log_node):
-
-        os.makedirs(log_dir, exist_ok=True)
-
-        self.log_dir = log_dir
-        self.log_node = log_node
-        self.df = pd.DataFrame()
-        self.tag = None
-        self.filepath = None
-        self.collector = ResourceMetricCollector(Device.cuda.all()).daemonize(
-            on_collect=self.on_collect,
-            interval=1.0,
-        )
-        self.cpu_count = psutil.cpu_count(logical=False)
-        self.start_time = None
-
-    def new_res(self):
-        """Returns the directory."""
-
-        os.makedirs(f"{self.log_dir}/{self.tag}", exist_ok=True)
-
-        self.filepath = f"{self.log_dir}/{self.tag}/resource-{self.log_node}.csv"
-
-    def change_tag(self, tag):
-        """Changes the tag."""
-        if self.filepath is not None:
-            self.stop()
-        self.tag = tag
-        self.new_res()
-
-    def stop(self):
-        """Stops the collector."""
-        if not self.df.empty:
-            self.df.to_csv(self.filepath, index=False)
-        self.df = pd.DataFrame()
-
-    def get_cpu_usage_per_core(self):
-        """Returns the CPU usage per core."""
-        cpu_percent = psutil.cpu_percent(interval=0.1, percpu=True)
-        return {f"cpu_core_{i+1}": percent for i, percent in enumerate(cpu_percent[:self.cpu_count])}
-
-    def clean_column_name(self, col):
-        """Cleans the column name."""
-        if col.startswith("metrics-daemon/host/"):
-            col = col[len("metrics-daemon/host/"):]
-        return col
-
-    def on_collect(self, metrics):
-        """Collects metrics."""
-
-        metrics['tag'] = self.tag
-
-        cpu_metrics = self.get_cpu_usage_per_core()
-        metrics.update(cpu_metrics)
-
-        df_metrics = pd.DataFrame.from_records([metrics])
-
-        df_metrics.columns = [self.clean_column_name(col) for col in df_metrics.columns]
-
-        if self.df.empty:
-            self.df = df_metrics
-        else:
-            for col in df_metrics.columns:
-                if col not in self.df.columns:
-                    self.df[col] = None
-
-            self.df = pd.concat([self.df, df_metrics], ignore_index=True)
-
-        return True
-
-
-class Logger:
-    """Arguments logger class."""
-
-    def __init__(self, log_dir):
-
-        os.makedirs(log_dir, exist_ok=True)
-
-        self.log_dir = log_dir
-
-    def log(self, obj, filename="log"):
-        """Logs the object."""
-        with open(f"{self.log_dir}/{filename}.json", "w", encoding='utf-8') as f:
-            json.dump(serialize(obj), f, ensure_ascii=False, indent=4)
 
 
 def train():
@@ -245,7 +144,7 @@ def train():
         "trainer_config": trainer.args.__dict__,
     }))
 
-    logger = Logger(log_dir=logging_args.log_dir)
+    logger = JSONLogger(log_dir=logging_args.log_dir)
 
     logger.log(all_dict, filename="arguments")
 
